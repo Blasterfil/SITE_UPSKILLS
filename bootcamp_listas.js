1e5259bf0083de7626dcc3694ed9283985b0d354
Bruno Oliveira <132902411+Ol1v31r4@users.noreply.github.com>
2023-07-04 13:58:54 +0100

    Update de candidatos e do bootcamp
    Update de candidatos com css e do bootcamp com jason e js para apresentar tabelas reativas

---
+++ bootcamp_listas.js
@@ -0,0 +1,24 @@
+function  fillTable_v2(){
+    /*Vai buscar a lista .JSON*/
+    fetch('bootcamp_listas.json')
+    /*RESPONSE e a var do conteudo da lista JSON*/    
+    .then(response => response.json())
+        /*DATA e a variavel que contem a informação JSON*/       
+        .then(data => {
+            
+            const bootcamp = data;
+            /*Inserir a informação na TABELA COM ID "tabela"*/
+            const tabela = document.getElementById("tabela");
+            /*Criar os TITULOS das COLUNAS*/
+            tabela.innerHTML = "<tr><th>Curso</th><th>Academias</th><th>Localizações</th><th>Estado</th></tr>";
+            /*PREENCHER A TABELA de acordo com os titulos enquanto houver info*/
+            for (let bootcamps of bootcamp){
+                tabela.innerHTML += `<tr><td>${bootcamps.Curso}</td><td>${bootcamps.Academia}</td><td>${bootcamps.Localização}</td><td>${bootcamps.Estado}</td></tr>`;
+            }
+        })
+
+    
+        .catch(error => {
+            console.error('Ocorreu um erro:', error);
+        });
+}
